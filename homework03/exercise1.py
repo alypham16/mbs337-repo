21576729dc34b51d1f0474ffc84d4522fa7177ff
alysa <apham16@utexas.edu>
2026-01-30 21:46:33 +0000

    committing exercise1 progress

---
+++ homework03/exercise1.py
@@ -0,0 +1,60 @@
+"""
+Exercise Prompt: Create a Python script called exercise 1.py that:
+
+1. Defines a ProteinEntry model with Pydantic
+2. Reads in the input JSON file as a Python Dictionary
+3. Converts the Python Dictionary into a list of ProteinEntry objects
+
+Then,
+
+1. Write a function called find_total_mass() that prints the total combined 
+mass of all proteins in the dataset.
+
+2. Write a function called find_large_proteins() that prints a list of protein 
+names of any proteins with a sequence length greater than or equal to 1000.
+
+3. Write a function called find_non_eukaryotes() that prints a list of protein 
+names of any non-eukaryotic proteins in the dataset. (Hint: A protein is considered 
+non-eukaryotic if “Eukaryota” does not appear in protein.organism['lineage'])
+
+After defining your functions, call each one so the results are printed to the 
+terminal when the script runs.
+
+"""
+import json
+from pydantic import BaseModel
+
+class ProteinEntry(BaseModel): # template describing structure of any protein entry
+    primaryAccession: str
+    organism: dict
+    proteinName: str
+    sequence: dict
+    geneName: str
+
+proteins = []
+with open("protein_list.json", "r") as f:
+    prot_data = json.load(f)
+
+for prot in prot_data["protein_list"]:
+    proteins.append(ProteinEntry(**prot)) # unpacking dictionary to turn into ProteinEntry object
+
+
+## this kind of math but need to access correctly
+def find_total_mass():
+    for prot in proteins:
+        total_mass += prot["mass"]
+    print(total_mass)
+
+## this kind of math but need to access correctly
+def find_large_proteins():
+    lg_prots = []
+    for prot in proteins:
+        if prot.length >= 1000:
+            lg_prots.append[prot]
+
+## this kind of math but need to access correctly
+def find_non_eukaryotes():
+    non_eukarya = []
+    for prot in proteins:
+        if prot.organism["lineage"] != "Eukaryota":
+            non_eukarya.append[prot]
